14e640cd63c70e834a5f873f4ecd8238329744c6
duffaldri <mdaffaaldriantama@gmail.com>
2023-05-29 09:16:51 +0700

    Added recipe details page. Have a scrolling problem (Because of nested column)

--- app/src/main/java/com/bangkit/vegalicious/models/Recipe.kt
+++ app/src/main/java/com/bangkit/vegalicious/models/Recipe.kt
@@ -1,22 +1,34 @@
 package com.bangkit.vegalicious.models
 
-data class RecipeCard(
+data class Recipe(
 	val id: String,
 	val title: String,
 	val photoUrl: String,
 	val tags: List<String>,
+	val nutrition: Map<String, Float>,
 	val description: String,
 )
 
 val dummyRecipeCards = List(30) { index ->
-	RecipeCard(
+	Recipe(
 		id = index.toString(),
 		title = "Resep $index",
 		photoUrl = "https://assets.epicurious.com/photos/63b5b03305dd27a0d03a18a6/1:1/w_1920,c_limit/Jackfruit%20curry-RECIPE.jpg",
 		tags = listOf(
-			"Tags 1",
-			"Tags 2",
-			"Tags 3",
+			"Indian",
+			"Indonesian",
+			"Gluten-Free",
+			"Vegetarian",
+			"Soy-Free",
+			"Curry",
+			"Western",
+			"Eastern"
+		),
+		nutrition = mapOf(
+			"fat" to 1234.2f,
+			"calories" to 23.2f,
+			"protein" to 334.2f,
+			"sodium" to 1110f,
 		),
 		description = "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet."
 	)
